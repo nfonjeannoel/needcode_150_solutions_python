be6ee10dcfe548f05904257e15991c0a8272159f
JEANNOEL <nfonjeannoel1@gmail.com>
2025-08-20 14:54:10 -0400

    linked-list-cycle-detection.py

---
+++ linked-list-cycle-detection.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        cur = head
+        while cur:
+            if cur in seen:
+                return True
+            seen.add(cur)
+            cur = cur.next
+        return False
+
+class Solution2:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+if __name__ == "__main__":
+    # Example usage:
+    node1 = ListNode(3)
+    node2 = ListNode(2)
+    node3 = ListNode(0)
+    node4 = ListNode(-4)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2  # Creates a cycle here
+
+    solution = Solution()
+    print(solution.hasCycle(node1))  # Output: True
+
+    nodeA = ListNode(1)
+    nodeB = ListNode(2)
+
+    nodeA.next = nodeB
+    nodeB.next = None  # No cycle
+
+    print(solution.hasCycle(nodeA))  # Output: False
+
+
