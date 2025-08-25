4ca75ba87453538ed890babff2c0cf15549ce672
JEANNOEL <nfonjeannoel1@gmail.com>
2025-08-25 11:55:39 -0400

    level-order-traversal-of-binary-tree.py

--- level-order-traversal-of-binary-tree.py
+++ level-order-traversal-of-binary-tree.py
@@ -30,3 +30,20 @@ class Solution:
                 res.append(level)
 
         return res
+
+
+from collections import deque
+class Solution2:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        def dfs(node, depth):
+            if not node:
+                return None
+            if len(res) == depth:
+                res.append([])
+            res[depth].append(node.val)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+        dfs(root, 0)
+        return res
