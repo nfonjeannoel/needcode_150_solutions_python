a58332aa85ae891e37f327fe27c17d251fef4b37
JEANNOEL <nfonjeannoel1@gmail.com>
2025-08-28 12:08:10 -0400

    kth-largest-integer-in-a-stream.py

---
+++ kth-largest-integer-in-a-stream.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import List
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+import heapq
+class KthLargest2:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k, self.nums = k, nums
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort()
+        return self.nums[len(self.nums) - self.k]
+
+
