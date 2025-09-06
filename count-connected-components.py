e68b0fc4142cdcd6a95bace781c172bbd713b4a7
JEANNOEL <nfonjeannoel1@gmail.com>
2025-09-06 11:37:37 -0400

    count-connected-components.py

--- count-connected-components.py
+++ count-connected-components.py
@@ -28,3 +28,25 @@ class Solution:
                         q.append(nb)
 
         return count
+
+class Solution2:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(node):
+            for nei in adj[node]:
+                if not visit[nei]:
+                    visit[nei] = True
+                    dfs(nei)
+
+        res = 0
+        for node in range(n):
+            if not visit[node]:
+                visit[node] = True
+                dfs(node)
+                res += 1
+        return res
