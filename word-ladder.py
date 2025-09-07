ee8ad339fa094cd35e78ae7c6cd768bf50663278
JEANNOEL <nfonjeannoel1@gmail.com>
2025-09-07 00:01:44 -0400

    word-ladder.py

---
+++ word-ladder.py
@@ -0,0 +1,32 @@
+from collections import defaultdict, deque
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        neb = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1:]
+                neb[pattern].append(word)
+
+        visit = set([beginWord])
+        q = deque([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for nebWord in neb[pattern]:
+                        if nebWord not in visit:
+                            visit.add(nebWord)
+                            q.append(nebWord)
+            res += 1
+        return 0
