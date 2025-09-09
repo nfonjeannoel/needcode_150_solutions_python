8632b7af083e817ab07950c1204f77ff8c6b271f
JEANNOEL <nfonjeannoel1@gmail.com>
2025-09-09 19:12:07 -0400

    maximum-subarray.py

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -11,3 +11,25 @@ class Solution:
             maxSub = max(maxSub, curSum)
 
         return maxSub
+
+    class Solution2:
+        def maxSubArray(self, nums: List[int]) -> int:
+            maxSum = nums[0]
+            for i in range(len(nums)):
+                curSum = 0
+                for j in range(i, len(nums)):
+                    curSum += nums[j]
+                    maxSum = max(maxSum, curSum)
+            return maxSum
+
+    class Solution3:
+        def maxSubArray(self, nums: List[int]) -> int:
+            curSum, maxSum = nums[0], nums[0]
+            for num in nums[1:]:
+                curSum = max(num, curSum + num)
+                maxSum = max(curSum, maxSum)
+            return maxSum
+
+
+
+
