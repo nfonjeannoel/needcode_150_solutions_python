6609a3945ebbb4174c8b4639ce2cfb4563a1acc5
JEANNOEL <nfonjeannoel1@gmail.com>
2025-09-20 16:09:44 -0400

    longest-common-subsequence.py

---
+++ longest-common-subsequence.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[0][0]
+
+
+class Solution2:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def dfs(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if text1[i] == text1[j]:
+                return 1 + dfs(i + 1, j + 1)
+            return max(dfs(i + 1, j), dfs(i, j + 1))
+
+        return dfs(0, 0)
+
+class Solution3:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+        def dfs(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if text1[i] == text2[j]:
+                memo[(i, j)] = 1 + dfs(i + 1, j + 1)
+            else:
+                memo[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
+            return memo[(i, j)]
+        return dfs(0, 0)
